a8a7f48bdcb34d71841c745839ee84a8e867dcb7
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-06-30 22:22:57 +0900

    feat: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <v-toolbar flat>
+      <v-toolbar-title>
+        <v-avatar size="30px">
+          <v-img :src="require('~/assets/logos/cryptorage.png')" alt="avatar"></v-img>
+        </v-avatar>
+        <span class="grey--text">CRYPTO</span>
+        <span class="orange--text">RAGE</span>
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-parallax :src="require('~/assets/logos/crypto-currency.jpg')" height="600">
+      <v-layout column align-center justify-center class="white--text">
+        <v-flex xs1>
+          <v-img :src="require('~/assets/logos/cryptorage.png')" width="150" height="150"></v-img>
+        </v-flex>
+        <h1 class="white--text mb-2 display-1 text-xs-center">Cryptorage</h1>
+        <div class="subheading text-xs-center">Crypto Asset Arbitrage Tool for Humansy</div>
+
+        <v-btn class="orange mt-5" dark large disabled to="/markets">Get Started</v-btn>
+      </v-layout>
+    </v-parallax>
+    <section>
+      <v-layout column wrap class="my-5" align-center>
+        <v-flex xs12 sm4 class="my-3">
+          <div class="text-xs-center">
+            <h2 class="headline">The best way to start developing</h2>
+            <span class="subheading">Cras facilisis mi vitae nunc</span>
+          </div>
+        </v-flex>
+        <v-flex xs12>
+          <v-container grid-list-xl>
+            <v-layout row wrap align-center>
+              <v-flex xs12 md4>
+                <v-card class="elevation-0 transparent">
+                  <v-card-text class="text-xs-center">
+                    <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
+                  </v-card-text>
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline text-xs-center">Material Design</div>
+                  </v-card-title>
+                  <v-card-text>
+                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla
+                    volutpat tincidunt ornare. Pellentesque habitant morbi
+                    tristique senectus et netus et malesuada fames ac turpis
+                    egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus
+                    bibendum tincidunt. Suspendisse potenti.
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+              <v-flex xs12 md4>
+                <v-card class="elevation-0 transparent">
+                  <v-card-text class="text-xs-center">
+                    <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                  </v-card-text>
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline">Fast development</div>
+                  </v-card-title>
+                  <v-card-text>
+                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla
+                    volutpat tincidunt ornare. Pellentesque habitant morbi
+                    tristique senectus et netus et malesuada fames ac turpis
+                    egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus
+                    bibendum tincidunt. Suspendisse potenti.
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+              <v-flex xs12 md4>
+                <v-card class="elevation-0 transparent">
+                  <v-card-text class="text-xs-center">
+                    <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+                  </v-card-text>
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline text-xs-center">Completely Open Sourced</div>
+                  </v-card-title>
+                  <v-card-text>
+                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla
+                    volutpat tincidunt ornare. Pellentesque habitant morbi
+                    tristique senectus et netus et malesuada fames ac turpis
+                    egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus
+                    bibendum tincidunt. Suspendisse potenti.
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "index"
+};
+</script>
+<style></style>
